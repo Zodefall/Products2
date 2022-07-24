f678558cef65ae045b277830a8fd518f5a018223
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-07-24 21:04:27 +0300

    Hide secret information

--- Drf/Drf/settings.py
+++ Drf/Drf/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_rr$m()7q@_96v10jq(tc^rlb(sf5ebw7zsm7zuj*2ir5b5!m-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,9 +85,9 @@ WSGI_APPLICATION = 'Drf.wsgi.application'
 DATABASES= {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Products',
-        'USER': 'user1',
-        'PASSWORD': '12345678',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
