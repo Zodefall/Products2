cd4952b752db0f9c1adb976b2cada862d5f5072a
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-07-25 20:10:59 +0300

    first commit for all files

--- Drf/Drf/settings.py
+++ Drf/Drf/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuappname.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
