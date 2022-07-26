63031f09a1e76510355cf4e1944c26945dbb3ccd
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-07-26 16:09:31 +0300

    Init git

--- Drf/Drf/settings.py
+++ Drf/Drf/settings.py
@@ -63,12 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 ROOT_URLCONF = 'Drf.urls'
 
@@ -141,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
