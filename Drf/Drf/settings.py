4d6315fe78f97540dc2bb3f0f74b8581bc79f7c1
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-07-28 12:23:44 +0300

    Test

--- Drf/Drf/settings.py
+++ Drf/Drf/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-# import django_heroku
+import django_heroku
 import environ
 import dj_database_url
 
@@ -160,4 +160,4 @@ SPECTACULAR_SETTINGS = {
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
+django_heroku.settings(locals())
