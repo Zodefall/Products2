1dae1c131516bb867e56cdf2501ecb9ebfa34fea
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-07-28 13:21:03 +0300

    Test

--- Drf/Drf/settings.py
+++ Drf/Drf/settings.py
@@ -161,6 +161,6 @@ SPECTACULAR_SETTINGS = {
     # OTHER SETTINGS
 }
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-django_heroku.settings(locals())
+# prod_db = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
+# django_heroku.settings(locals())
