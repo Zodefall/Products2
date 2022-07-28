0157e4e073e159780d3ca787b38b5e03f8dca0d6
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-07-28 12:20:57 +0300

    Test

--- Drf/Drf/settings.py
+++ Drf/Drf/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import django_heroku
+# import django_heroku
 import environ
 import dj_database_url
 
@@ -22,7 +22,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
