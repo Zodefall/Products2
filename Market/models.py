21ab3ccac76944702049895131df66f9e76dfdb6
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-08-10 02:10:21 +0300

    add user avatar

--- Market/models.py
+++ Market/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.PROTECT)
+    user = models.OneToOneField(User,on_delete=models.PROTECT,upload_to='photos/%Y/%m/%d')
     avatar = models.ImageField()
 
     def __str__(self):
