7be628765e6a9086e944c79b8fb4153a2e406ff0
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-08-10 01:34:45 +0300

    add user avatar

--- Market/admin.py
+++ Market/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from Market.models import Market, Category,UserProfile
+from Market.models import Market, Category, UserProfile
 
 admin.site.register(Market)
 admin.site.register(Category)
--- Market/models.py
+++ Market/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete= models.CASCADE())
+    user = models.OneToOneField(User)
     avatar = models.ImageField()
 
     def __str__(self):
