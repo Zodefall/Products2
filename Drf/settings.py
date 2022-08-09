528b21a24a0e8c268549676678e5b6c786dbd6e3
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-08-10 01:52:37 +0300

    add user avatar

--- Drf/settings.py
+++ Drf/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_rr$m()7q@_96v10jq(tc^rlb(sf5ebw7zsm7zuj*2ir5b5!m-'#env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -164,7 +164,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
-        'rest_framework.authentication.BasicAuthentication'
+
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
